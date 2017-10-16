754a6a16712a6f3affbd32b871c1486031c04c3e
Claudio Romano <claudiocro@gmail.com>
2017-10-17 00:31:13 +0200

    test: ignore untestable line

--- src/i18n.js
+++ src/i18n.js
@@ -46,6 +46,7 @@ export default class VueI18n {
 VueI18n.install = install;
 VueI18n.version = __VERSION__;
 
+/* istanbul ignore if */
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(VueI18n);
 }
